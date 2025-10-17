3c9644a98749c7e83da93abefd6fe7ae056bf480
ClaudioGitt <claudimjose@hotmail.com>
2025-10-17 16:39:45 -0300

    Meu primeiro commit. Aula do dia 17/10/2025

---
+++ 01_logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,15 @@
+# declacação de variáveis
+nome_completo = "Claudio oliveira" # Os dados armazenados na variável nome_completo é um tipo string
+
+# tipo numero inteiro
+idade = 37 # string
+altura = 1.73 # float
+programador = True # Dados booleanos
+faculdade = False
+
+# saída de dados
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
+print(faculdade)
