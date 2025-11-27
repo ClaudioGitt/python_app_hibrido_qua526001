55a6a460bba27cc257dd0b44ecc5b864eb3c9a2e
ClaudioGitt <claudimjose@hotmail.com>
2025-11-27 17:31:38 -0300

    Aula 27/11/2025 código revisado

--- 07_app_hibrido/04_flex_fuel/src/main.py
+++ 07_app_hibrido/04_flex_fuel/src/main.py
@@ -1,8 +1,7 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-        def calcular_combustivel(e):
+      def calcular_combustivel(e):
             if not gasolina.value:
                   gasolina.error_text = "Campo não pode ficar vazio"
                   page.update()
@@ -14,39 +13,41 @@ def main(page: ft.Page):
                   page.update()
             else:
                   etanol.error_text = ""
-                  gasolina.value=float(gasolina.value)
-                  etanol.value=float(etanol.value)
+                  gasolina.value = float(gasolina.value.replace(",","."))
+                  etanol.value = float(etanol.value.replace(",","."))
                   resultado = "gasolina" if etanol.value >= gasolina.value * 0.7 else "etanol"
                   # resultado com caixa de diálogo
-                  dlg_modal.content.value=resultado
+                  dlg_modal.content.value = resultado
                   gasolina.value = ""
                   etanol.value = ""
+                  
 
                   page.open(dlg_modal)
 
                   page.update()
-        page.title = "APP Flex Fuel"
-        page.scroll = "adaptive"
-        page.theme_mode = ft.ThemeMode.LIGHT
+      page.title = "APP Flex Fuel"
+      page.scroll = "adaptive"
+      page.theme_mode = ft.ThemeMode.LIGHT
 
-        gasolina = ft.TextField(label="Valor da gasolina",prefix_text="R$ ",keyboard_type=ft.KeyboardType.NUMBER,hover_color="#add8e6",bgcolor="white")
+      gasolina = ft.TextField(label="Valor da gasolina",prefix_text="R$ ",keyboard_type=ft.KeyboardType.NUMBER,hover_color="#add8e6",bgcolor="white")
 
-        etanol = ft.TextField(label="Valor da etanol",prefix_text="R$ ",keyboard_type=ft.KeyboardType.NUMBER,hover_color="#add8e6",bgcolor="white",on_submit=calcular_combustivel)
+      etanol = ft.TextField(label="Valor da etanol",prefix_text="R$ ",keyboard_type=ft.KeyboardType.NUMBER,hover_color="#add8e6",bgcolor="white",on_submit=calcular_combustivel)
 
-        dlg_modal = ft.AlertDialog(modal=True,title=ft.Text("Melhor abastecer com: "))
-        content=ft.Text(size=20, width="bold")
+      dlg_modal = ft.AlertDialog(modal=True,
+                              title=ft.Text("Melhor abastecer com: "),content=ft.Text(size=20,
+                              width="bold"),
+                              actions = [ft.TextButton("OK",
+                              on_click=lambda e:page.close((dlg_modal)))],
+                              actions_alignment = ft.MainAxisAlignment.END)
 
-        actions = [ft.TextButton("OK", on_click=lambda e:page.close((dlg_modal)))],
-        actions_alignment=ft.MainAxisAlignment.END
-        gasosa = ft.Text()
 
-        page.add(
+      page.add(
             ft.SafeArea(
                   ft.Container(
                         ft.Text("FLEX FUEL", size=25, weight="bold",color=""),
                         alignment=ft.alignment.center,
+                        padding=100
                   ),
-                  expand=True,
             ),
             ft.ResponsiveRow(
                   [ # dicionario, informando chaves e valores.
@@ -63,9 +64,7 @@ def main(page: ft.Page):
                               on_hover="#ADD8E6"
                         )
                   ],
-                  alignment=ft.MainAxisAlignment.CENTER
+                  alignment=ft.MainAxisAlignment.CENTER 
             )
-    )
-
-
+      )
 ft.app(main)
