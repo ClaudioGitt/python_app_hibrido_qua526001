7d4b7461d958b2ce8705a3f2cecaba47a52b639f
ClaudioGitt <claudimjose@hotmail.com>
2025-12-09 17:20:47 -0300

    Aula 09/12/2025 flask form

--- 08_flask/02_form_flask/main.py
+++ 08_flask/02_form_flask/main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/enviar")
+@app.route("/enviar", methods=["POST"])
 def enviar():
     # como estamos pegando dados de um formulário, essa é a maneira.
-    texto = request.form.get('Texto')
-    numero = request.form.get('Numero')
-    return render_template("index.html", nome=texto,idade=numero)
+    texto = request.form.get('texto', '')
+    idade = request.form.get('idade', '')
+    return render_template("index.html", saida=texto, saida2 = idade)
 if __name__ == "__main__":
     app.run(debug=True)
